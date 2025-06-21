50dd78284934de93191409a07a69157073b31ca3
andypavelka <andy@onepercent.cz>
2025-06-21 15:12:58 +0200

    Update vokativ_api.py

--- vokativ_api.py
+++ vokativ_api.py
@@ -3,8 +3,31 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-vokativ_dict = {"Jan": "Jane", "Petr": "Petře", "Lucie": "Lucie", "Tomáš": "Tomáši", "Ondřej": "Ondřeji", "Vratislav": "Vratislave", "Eva": "Evo", "Filip": "Filipe", "Tereza": "Terezo", "Radek": "Radku", "Dominik": "Dominiku", "Daniel": "Daniele", "Kateřina": "Kateřino", "Lenka": "Lenko", "David": "Davide", "Veronika": "Veroniko", "Matěj": "Matěji", "Barbora": "Barboro", "Karel": "Karle"}
+vokativ_dict = {
+    # Mužská jména
+    "Jan": "Jane", "Petr": "Petře", "Pavel": "Pavle", "Tomáš": "Tomáši",
+    "Jiří": "Jiří", "Martin": "Martine", "Miroslav": "Miroslave", "Jaroslav": "Jaroslave",
+    "František": "Františku", "Josef": "Josefe", "David": "Davide", "Michal": "Michale",
+    "Lukáš": "Lukáši", "Jakub": "Jakube", "Milan": "Milane", "Václav": "Václave",
+    "Daniel": "Daniele", "Adam": "Adame", "Ondřej": "Ondřeji", "Radek": "Radku",
+    "Marek": "Marku", "Roman": "Romane", "Aleš": "Aleši", "Zdeněk": "Zdeňku",
+    "Stanislav": "Stanislave", "Robert": "Roberte", "Richard": "Richarde",
+    "Filip": "Filipe", "Vlastimil": "Vlastimile", "Ladislav": "Ladislave",
+    "Antonín": "Antoníne", "Libor": "Libore", "Vojtěch": "Vojtěchu",
+    "Karel": "Karle", "Jindřich": "Jindřichu", "Bohuslav": "Bohuslave",
+    "Rostislav": "Rostislave", "Bohumil": "Bohumile", "Oldřich": "Oldřichu",
 
+    # Ženská jména
+    "Jana": "Jano", "Marie": "Marie", "Eva": "Evo", "Anna": "Anno",
+    "Hana": "Hano", "Lenka": "Lenko", "Kateřina": "Kateřino", "Věra": "Věro",
+    "Alena": "Aleno", "Petra": "Petro", "Veronika": "Veroniko", "Tereza": "Terezo",
+    "Martina": "Martino", "Michaela": "Michaelo", "Jiřina": "Jiřino", "Božena": "Boženo",
+    "Helena": "Heleno", "Zuzana": "Zuzano", "Barbora": "Barbaro", "Kristýna": "Kristýno",
+    "Monika": "Moniko", "Pavla": "Pavlo", "Lucie": "Lucie", "Ivana": "Ivano",
+    "Dagmar": "Dagmar", "Jitka": "Jitko", "Andrea": "Andreo", "Radka": "Radko",
+    "Markéta": "Markéto", "Simona": "Simono", "Klára": "Kláro", "Nikola": "Nikolo",
+    "Irena": "Ireno", "Milada": "Milado", "Růžena": "Růženo", "Vlasta": "Vlasto"
+}
 @app.route('/api/vokativ')
 def get_vokativ():
     name = request.args.get('name', '').strip()
